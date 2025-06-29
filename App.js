6ccf1f3e604c3372227b70f3a4592e52d908e783
samWidlund <sam.wid415@gmail.com>
2025-06-29 15:12:30 +0200

    testing changes

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>elevationW</Text>
+      <Text>Running app for generating elevation based trail routes</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2E7D32',
     alignItems: 'center',
     justifyContent: 'center',
   },
