bf726cf24b558197c4b8dc3a639dd7a5b3711038
samWidlund <sam.wid415@gmail.com>
2025-07-03 07:59:30 +0200

    replace bg color from json to js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import "./global.css"
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1 items-center justify-center bg-white">
       <StatusBar style="auto"/>
       
     <View className="shadow-lg bg-white rounded-3xl mb-5">
